8c3883250d070413494bf22c6dd3039b0565ea91
ezurno <ezurno@gmail.com>
2023-05-03 21:29:59 +0900

    SET: set test page
    1. NEXT.js 의 장점을 알아보기 위한 test page
    
    2. NEXT 는 CRA 와는 다르게 웹페이지를 조립 한 후 뿌려주기 때문에 사용자의 인터넷 속도, JS 동작 여부와는 상관없이 html 을 정상적으로 보여줌

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+
 export default function Home() {
-  return "HI";
+  const [counter, setCounter] = useState(0);
+
+  const upCounter = () => {
+    setCounter((prev) => prev + 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter {counter}</h1>
+      <button onClick={upCounter}>+</button>
+    </div>
+  );
 }
