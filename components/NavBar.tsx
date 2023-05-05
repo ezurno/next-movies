45fbb8d10031ee68b6e9ad0894b7e6d9690ec2b2
ezurno <ezurno@gmail.com>
2023-05-05 19:07:20 +0900

    LEARN: css JSX
    1. JSX 방식으로 style 을 부여하는 방법
    
    2. module 방식보다 쓰기 편하지만 전역으로 쓰기는 문제가 있음
    
    3. 따라서 전역방식은 App 을 이용한 방식을 사용할 것을 권장

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,31 +1,20 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
-  const router = useRouter();
-  // Next.js 내 route 를 관리하는 Hook, console.log 를 찍어보면 상세 정보를 알 수 있음
-  // console.log(router);
   return (
-    <nav>
-      <Link
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-        href="/"
-      >
-        Home
-      </Link>
-      <Link
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join(" ")}
-        href="/about"
-      >
-        About
-      </Link>
-    </nav>
+    <>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <style jsx global>{`
+          a {
+            text-decoration: none;
+          }
+          nav {
+            background-color: tomato;
+          }
+        `}</style>
+      </nav>
+    </>
   );
-  // Link Component 는 anchor 대신 사용함 (re-rendering 에 유용)
 }
