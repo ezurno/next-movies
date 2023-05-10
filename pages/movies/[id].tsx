73e48b2640f711c33a044c4cdec4be905cccecfa
ezurno <ezurno@gmail.com>
2023-05-10 23:57:03 +0900

    ADD: movie-detail

--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -5,12 +5,47 @@ import { useRouter } from "next/router";
 export default function Detail({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
   return (
     <div>
       <Helmet title={data.title} />
-      <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} />
+      <div className="container">
+        <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} />
+        <h1>
+          {data.title} ({data.runtime} MIN)
+        </h1>
+        <h4>개봉일 : {data.release_date}</h4>
+        <h4>평점 : {data.vote_average.toFixed(2)}점</h4>
+        <p>{data.overview}</p>
+      </div>
 
-      <h4>{data.title}</h4>
+      <style jsx>
+        {`
+          .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+          img {
+            margin: 0 auto;
+            border-radius: 12px;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+            height: 500px;
+          }
+
+          h1,
+          h4 {
+            text-align: center;
+          }
+          h4 {
+            font-size: 18px;
+          }
+          p {
+            width: 500px;
+            text-align: center;
+          }
+        `}
+      </style>
     </div>
   );
 }
