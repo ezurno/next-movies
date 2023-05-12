a416640bd57c8190c59639c07537bc79900376e0
ezurno <ezurno@gmail.com>
2023-05-12 20:36:11 +0900

    ADD: about page
    1. about page 에 사용한 tool 정보를 담음
    
    2. shields 에서 이미지를 받아옴

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,37 @@
 import Helmet from "@/components/Helmet";
+import css from "styled-jsx/css";
+
+const styled = css`
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    padding: 30px 0px;
+
+    height: auto;
+    width: 100%;
+  }
+
+  .container img {
+    height: 100px;
+    width: 100%;
+  }
+`;
 
 export default function about() {
   return (
-    <div>
+    <>
       <Helmet title="about" />
-      <h1>About</h1>
-    </div>
+      <div className="container">
+        <h1>🛠️ Tools 🛠️</h1>
+        <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=white" />
+        <img src="https://img.shields.io/badge/TypeScript-3178C6?style=for-the-badge&logo=TypeScript&logoColor=white" />
+        <img src="https://img.shields.io/badge/ReactQuery-FF4154?style=for-the-badge&logo=React-Query&logoColor=white" />
+        <img src="https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=Next.js&logoColor=white" />
+      </div>
+
+      <style jsx>{styled}</style>
+    </>
   );
 }
